6444bc090ff4b5f924cebf694444c846366fe6f2
callistagloss <49454796+callistagloss@users.noreply.github.com>
2019-04-27 17:41:26 -0400

    Add files via upload

---
+++ Capstone Week 1 Final/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Capstone_1
+{
+    public class Program
+
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grand Circus Pig Latin Translator!\n");
+            PromptUser();
+        }
+
+        public static Boolean Continue()
+        {
+            Console.WriteLine("Continue? y/n");
+            string input = Console.ReadLine();
+            Boolean run = false;
+            input.ToLower();
+
+            if (input == "n")
+            {
+                Console.WriteLine("Goodbye.");
+                run = false;
+            }
+            else if (input == "y")
+            {
+                run = true;
+                PromptUser();
+            }
+            else
+            {
+                Console.WriteLine("I'm sorry. I didn't understand your input. Let's try that again.");
+                run = Continue();
+               
+            }
+            return run;
+        }
+
+
+        public static void PromptUser()
+        {
+            Console.WriteLine("Please enter a word you would like to translate:");
+            string input = Console.ReadLine().ToLower();
+
+            char firstLetter = input[0];
+
+            if (firstLetter == 'a' || firstLetter == 'e' || firstLetter == 'i' || firstLetter == 'o' || firstLetter == 'u')
+            {
+                VowelBuilder(input);
+            }
+            else if (!input.Contains("a") && !input.Contains("e") && !input.Contains("i") && !input.Contains("o") && !input.Contains("u"))
+            {
+                VowelBuilder(input);
+            }
+            else
+            {
+                ConsonantBuilder(input);
+            }
+
+        }
+
+        public static void VowelBuilder(string input)
+        {
+            Console.WriteLine(input + "way");
+            Continue();
+        }
+
+        public static void ConsonantBuilder(string input)
+        {
+            string word = input;
+            string[] vowels = { "a", "e", "i", "o", "u" };
+            List<int> foundVowels = new List<int>();
+
+            foreach (string letter in vowels)
+            {
+                int index = word.IndexOf(letter);
+
+                if (index > 0)
+                {
+                    foundVowels.Add(index);
+                }
+            }
+
+            int firstVowel = 1000;
+
+            foreach (int vowelPositions in foundVowels)
+            {
+                if (vowelPositions < firstVowel)
+                {
+                    firstVowel = vowelPositions;
+                }
+            }
+
+            string secondHalf = word.Substring(firstVowel);
+            string firstHalf = word.Substring(0, firstVowel);
+            string pigString = secondHalf + firstHalf + "ay";
+            Console.WriteLine(pigString);
+            Continue();
+        }
+    }
+}
